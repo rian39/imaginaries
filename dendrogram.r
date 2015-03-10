cec45453c810ea119cc06e46ceb0cb882381f13c
rian39 <a.mackenzie@lancaster.ac.uk>
2015-03-10 08:21:17 +0000

    update

--- dendrogram.r
+++ dendrogram.r
@@ -1,31 +1,35 @@
 library('RCircos')
+library('rpart')
 library(ape)
 library(cluster)
 library(network)
 library(igraph)
 data(mtcars)
-plot(as.phylo(hclust(dist(mtcars))),type="fan")
-daisy(mtcars)
-plot(as.phylo(hclust(daisy(mtcars))),type="fan")
+#plot(as.phylo(hclust(dist(mtcars))),type="fan")
+#daisy(mtcars)
+#plot(as.phylo(hclust(daisy(mtcars))),type="fan")
 setwd('~/Documents/imaginaries_cesagen_2010/')
 imag = read.csv('imaginary_data.csv')
 row.names(imag) = imag$author
-imag = imag[,-1]
+#row.names(imag) = imag$term
+#imag = imag[,-1]
 plot(as.phylo(hclust(daisy(imag, metric='gower'))),type="fan")
+plot(as.phylo(hclust(daisy(imag, metric='gower'))))
+
 #svg('dendrogram.svg')
 #plot(as.phylo(hclust(daisy(imag, metric='gower'))),type="fan")
 #dev.off()
 #plot(as.phylo(hclust(daisy(imag, metric='gower'))))
-     if (require(network)) {
-         x <- as.network(rtree(10))
-         print(x)
-         plot(x, vertex.cex = 1:4)
-         plot(x, displaylabels = TRUE)
-     }
-     tr <- rtree(5)
-     if (require(igraph)) {
-         print((x <- as.igraph(tr)))
-         plot(x)
-         print(as.igraph(tr, TRUE, FALSE))
-         print(as.igraph(tr, FALSE, FALSE))
-     }
+     #if (require(network)) {
+         #x <- as.network(rtree(10))
+         #print(x)
+         #plot(x, vertex.cex = 1:4)
+         #plot(x, displaylabels = TRUE)
+     #}
+     #tr <- rtree(5)
+     #if (require(igraph)) {
+         #print((x <- as.igraph(tr)))
+         #plot(x)
+         #print(as.igraph(tr, TRUE, FALSE))
+         #print(as.igraph(tr, FALSE, FALSE))
+     #}
